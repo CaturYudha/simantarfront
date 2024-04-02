86a84d93a39e62cc4f1f346eed7fbc89625020de
Jenifla <jeniflanurhidayah@gmail.com>
2024-04-02 14:19:12 +0700

    Mengubah login menjadi menggunakan jwt

--- src/login/login.js
+++ src/login/login.js
@@ -1,40 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./login.css";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 function Login() {
-  const [loginFormData, setLoginFormData] = useState({
-    username: "",
-    password: "",
-  });
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLoginChange = (e) => {
-    setLoginFormData({ ...loginFormData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    if (name === "username") {
+      setUsername(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
   };
+
+  useEffect(() => {
+  if(localStorage.getItem('token')){
+    window.location.href = "/dashboard";
+  }
+  fetchData();
+}, []);
+
 
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
-
-    const { username, password } = loginFormData;
-
     try {
+      const formData = new FormData;
+      formData.append('username', username);
+      formData.append('password', password);
       const response = await axios.post(
         "http://localhost:8000/api/auth/login",
-        {
-          username: username,
-          password: password,
-        }
+         formData,
+         { headers: { 'Content-Type': 'multipart/form-data' } }
       );
 
-      console.log(response.data); // Tampilkan respons dari backend (opsional)
+      console.log(response.data.access_token); // Tampilkan respons dari backend (opsional)
 
+      localStorage.setItem('token',response.data.access_token);
+      
+      await fetchData(); // Panggil fungsi fetchData untuk mengambil data pengguna (termasuk role) 
       alert("Login berhasil");
       // Redirect atau navigasi ke halaman lain jika diperlukan
       window.location.href = "/dashboard";
     } catch (error) {
       console.error("Error during login:", error);
       alert("Login gagal. Periksa kembali username dan password Anda");
+    }
+};
+
+  const fetchData = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      const response = await axios.post('http://localhost:8000/api/auth/me');
+      localStorage.setItem('role', response.data.role); // Simpan role ke dalam localStorage
+      localStorage.setItem('id', response.data.id);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
     }
   };
 
@@ -59,7 +83,7 @@ function Login() {
                 type="text" // Ubah tipe input menjadi text untuk username
                 name="username" // Ubah name menjadi username
                 placeholder="Username" // Ubah placeholder menjadi Username
-                value={loginFormData.username}
+                value={username}
                 onChange={handleLoginChange}
               />
               <i className="bx bxs-user"></i>
@@ -69,7 +93,7 @@ function Login() {
                 type="password"
                 name="password"
                 placeholder="Password"
-                value={loginFormData.password}
+                value={password}
                 onChange={handleLoginChange}
               />
             </div>
