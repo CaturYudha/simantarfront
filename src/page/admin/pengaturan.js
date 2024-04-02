cc0387fcdd005085b678c4a6d2da80f66fc3e73f
Jenifla <jeniflanurhidayah@gmail.com>
2024-04-02 14:16:18 +0700

    Menambahkan halaman pengaturan

---
+++ src/page/admin/pengaturan.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from "../../component/Slidebar.js";
+import Pengaturan from "../../pengaturan/pengaturan.js"
+
+const pengaturan = () => {
+  return (
+    <div>
+      <Sidebar/>
+      <Pengaturan/>
+    </div>
+  )
+}
+
+export default pengaturan
