2acc68a94eca96abc1e7d8f644b8c48be4fbc456
Jenifla <jeniflanurhidayah@gmail.com>
2024-04-02 14:15:42 +0700

    Menambahkan halaman laporan

---
+++ src/page/admin/laporan.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../../component/Slidebar.js";
+import Laporan from "../../laporan/laporan.js";
+
+const laporan = () => {
+  return (
+    <div>
+       
+      <Laporan />
+    </div>
+  );
+};
+
+export default laporan;
